cbf83268b621186e036724ae449e7bd421c8a901
Giovanny <giovanny.m.valencia@gmail.com>
2024-12-02 03:47:44 -0500

    Moved hardcoded UI strings -> strings.xml, cancel button returns to main

--- app/src/main/java/rutgers/pizzeria/androidapp/CreationActivity.java
+++ app/src/main/java/rutgers/pizzeria/androidapp/CreationActivity.java
@@ -1,12 +1,31 @@
 package rutgers.pizzeria.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreationActivity extends AppCompatActivity {
+
+    String selectedPizza = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creation); // Link to your XML layout
+        setContentView(R.layout.activity_creation);
+
+        Intent intent = getIntent();
+        selectedPizza = intent.getStringExtra("pizzaSelected");
+        TextView textViewSelectedPizza = findViewById(R.id.textViewSelectedPizza);
+        textViewSelectedPizza.setText(selectedPizza);
     }
+
+    //just returns to main activity
+    public void onCancel(View view){
+        finish();
+    }
+
 }
