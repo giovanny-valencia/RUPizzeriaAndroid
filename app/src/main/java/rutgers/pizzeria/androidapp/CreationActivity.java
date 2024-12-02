2a132c45f9fda0b59a735025f7e78a2e336e84f0
Giovanny <giovanny.m.valencia@gmail.com>
2024-12-02 01:05:28 -0500

    Transition from main -> creation activity, initial activity blue print for creation. Creation activity added to Manifest

---
+++ app/src/main/java/rutgers/pizzeria/androidapp/CreationActivity.java
@@ -0,0 +1,12 @@
+package rutgers.pizzeria.androidapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation); // Link to your XML layout
+    }
+}
--- app/src/main/java/rutgers/pizzeria/androidapp/MainActivity.java
+++ app/src/main/java/rutgers/pizzeria/androidapp/MainActivity.java
@@ -1,6 +1,11 @@
 package rutgers.pizzeria.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Link to the blank layout
         setContentView(R.layout.activity_main);
+    }
+
+    public void handlePizzaClick(View view) {
+        ImageButton imgBtn = (ImageButton) view;
+        String tag = (String) imgBtn.getTag(); // Read the tag
+        Toast.makeText(this, "Clicked: " + tag, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, CreationActivity.class);
+
+        intent.putExtra("pizzaSelected",tag);
+
+        startActivity(intent);
     }
 }
